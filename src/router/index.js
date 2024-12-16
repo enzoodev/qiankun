06aac4bdc44bba70df9c5d5971cbffe893d15560
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-12-16 12:38:40 -0300

    ✨ add meta title and subtitle to About page route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 const routes = [
-  { path: '/', name: 'home', component: () => import(/* webpackChunkName: "home" */ '@/views/Home') },
-  { path: '/about', name: 'about', component: () => import(/* webpackChunkName: "about" */ '@/views/About') },
+  {
+    path: '/', name: 'home', component: () => import(/* webpackChunkName: "home" */ '@/views/Home') },
+  { path: '/about', name: 'about', component: () => import(/* webpackChunkName: "about" */ '@/views/About'), meta: {
+      title: 'Meu title de meta',
+      subtitle: 'Meu subtitle de meta',
+  } },
 ];
 
 export default routes;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="about-content">
     <h1>This is about page</h1>
+    <h2>{{ metaTitle }}</h2>
+    <p>{{ metaSubtitle }}</p>
   </div>
 </template>
 
 <script>
+import { useRoute } from 'vue-router';
+
 export default {
   name: 'About',
-  setup() {},
+  setup() {
+    // Obter a rota atual
+    const route = useRoute();
+
+    // Retornar os dados do meta
+    return {
+      metaTitle: route.meta.title,
+      metaSubtitle: route.meta.subtitle,
+    };
+  },
 };
 </script>
 
